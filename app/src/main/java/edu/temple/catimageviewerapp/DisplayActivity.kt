ca42eaf45990efbfef7da6e2885d39d24e2dad81
SteveT90 <stevetolvaj@gmail.com>
2021-10-04 14:33:25 -0400

    Added image resources and renamed activity classes

--- app/src/main/java/edu/temple/catimageviewerapp/DisplayActivity.kt
+++ app/src/main/java/edu/temple/catimageviewerapp/DisplayActivity.kt
@@ -3,9 +3,9 @@ package edu.temple.catimageviewerapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class DisplayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_display)
     }
 }
---
+++ app/src/main/java/edu/temple/catimageviewerapp/SelectionActivity.kt
@@ -0,0 +1,14 @@
+package edu.temple.catimageviewerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+
+class SelectionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
