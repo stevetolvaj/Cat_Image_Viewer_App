22f92d6e83e2d6e84318ae0e31665f16ee6143f9
SteveT90 <stevetolvaj@gmail.com>
2021-10-19 14:51:56 -0400

    Updated to include display fragment add and commit

--- app/src/main/java/edu/temple/catimageviewerapp/MainActivity.kt
+++ app/src/main/java/edu/temple/catimageviewerapp/MainActivity.kt
@@ -7,13 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    // Save const string for put extra tag.
-    companion object {
-        const val EXTRA_IMAGE_OBJECT = "edu.temple.catimageviewerapp.IMAGE_OBJECT"
-        const val EXTRA_IMAGE_LIST = "edu.temple.catimageviewerapp.IMAGE_LIST"
-    }
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,22 +18,21 @@ class MainActivity : AppCompatActivity() {
         // Calling Factory Method to generate a new instance, passing in imageList to bundle
         // into fragment arguments.
         val selectionFragment = SelectionFragment.getInstance(imageList(resources))
+
+        if (savedInstanceState == null)
         supportFragmentManager.beginTransaction()
             .add(R.id.fragmentSelectionContainerView, selectionFragment)
+            .commit()
+
+        val displayFragment = DisplayFragment()
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentDisplayContainerView, displayFragment)
             .commit()
     }
 
 
-    /**
-     * The myOnClick function is used as a click listener and to start new activity when click occurs.
-     * @param position The position of child view when click occurred.
-     */
-    fun myOnClick(position: Int) {
-//        val intent = Intent(this, DisplayActivity::class.java).apply {
-//            putExtra(EXTRA_IMAGE_OBJECT, imageList(resources)[position])
-//        }
-        //startActivity(intent)
-    }
+
 
 }
 
